ea54a9ac59df46808717298df660ce6681f19ee5
Felix Delattre <felix@delattre.de>
2025-12-23 10:44:59 +0100

    Added ndvi for fusion.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from efast import run_efast, prepare_s2, prepare_s3
+from ndvi import generate_ndvi_fusion, create_ndvi_timeseries_fusion
 
 year = 2024
 site_position = (47.116171, 11.320308)
@@ -18,11 +19,16 @@ site_name = "innsbruck"
 # detect_clouds(year, site_name)
 # print("Cloud detection completed")
 
-print(f"Preparing data for EFAST fusion for {site_name}, {year}")
-prepare_s2(year, site_position, site_name)
-prepare_s3(year, site_position, site_name)
-print("Data preparation completed")
+# print(f"Preparing data for EFAST fusion for {site_name}, {year}")
+# prepare_s2(year, site_position, site_name)
+# prepare_s3(year, site_position, site_name)
+# print("Data preparation completed")
 
-print(f"Running EFAST fusion for {site_name}, {year}")
-run_efast(year, site_position, site_name)
-print("EFAST fusion completed")
+# print(f"Running EFAST fusion for {site_name}, {year}")
+# run_efast(year, site_position, site_name)
+# print("EFAST fusion completed")
+
+print(f"Generating NDVI for fusion outputs: {site_name}, {year}")
+generate_ndvi_fusion(year, site_position, site_name)
+create_ndvi_timeseries_fusion(year, site_position, site_name)
+print("Fusion NDVI generation completed")
